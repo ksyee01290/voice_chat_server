35a0c2909e203525aebc78c87eca1c2d7a9b3ded
source <sourcejung@gmail.com>
2023-02-09 15:56:41 +0900

    서버 클라간의 단일통신 test

--- client.py
+++ client.py
@@ -1,28 +1,44 @@
-import socket
+from socket import *
+import threading
+import time
 
+def send(socket):
+    while True:
+        senddata = input('>>>')
+        socket.send(senddata.encode())
+        
+def recve(socket):
+    while True:
+        recvdata = socket.recv(1024)
+        print("상대방",recvdata.decode())
+        
+        
 host ="127.0.0.1"
 port = 9999
 
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
+client_socket = socket(AF_INET,SOCK_STREAM)
 client_socket.connect((host,port))
+
+sender = threading.Thread(target = send, args = (client_socket,))
+recver = threading.Thread(target = recve, args = (client_socket,))
+
+sender.start()
+recver.start()
+
+while True:
+    time.sleep(1)
+    pass
+
+"""
+def recv_data(client_socket):
+    while True:
+        data = client_socket.recv(1024)
+        print("recv:",repr(data.decode()))
+
 
 while True:
     message = input('message : ')
     if message == 'exit':
         break
-    client_socket.send(message.encode())
-    data = client_socket.recv(1024)
-
-    print('recev from the server :',repr(data.decode()))
-
-client_socket.close()
-
-
-
-HOST = '127.0.0.1'
-PORT = 9999
-
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
-
-client_socket.connect((HOST, PORT)) 
+    
+    client_socket.send(message.encode())"""
--- server.py
+++ server.py
@@ -1,26 +1,35 @@
 import socket
-from _thread import *
+import threading
+import time
 
+def send(socket):
+    while True:
+        senddata = input('>>>')
+        socket.send(senddata.encode())
+        
+def recve(socket):
+    while True:
+        recvdata = socket.recv(1024)
+        print("상대방",recvdata.decode())
+        
+"""   
 def threaded(client_socket,addr):
     print('connect:',addr[0],":",addr[1])
     
     while True:
         try:
-            data = client_socket.recv(1024)
-            
+            data = client_socket.recv(1024)         
             if not data:
                 print("disconnect:" + addr[0],":",addr[1])
-                break
+                break          
             print("recvfrom" + addr[0],":",addr[1],data.decode())
-            client_socket.send(data)
             
         except ConnectionResetError as e:
-
             print('Disconnected by ' + addr[0],':',addr[1])
             break
              
     client_socket.close()
-            
+            """
 
 host = "127.0.0.1"
 
@@ -30,14 +39,20 @@ server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 server_socket.bind((host,port))
 server_socket.listen()
-print("server start")
 
-while True: 
+print("wait")
 
-    print('wait')
+connectionsocket, addr = server_socket.accept()
 
+print(str(addr),'에 접속되었습니다.')
 
-    client_socket, addr = server_socket.accept() 
-    start_new_thread(threaded, (client_socket, addr)) 
+sender = threading.Thread(target = send, args = (connectionsocket,))
+recver = threading.Thread(target = recve, args = (connectionsocket,))
 
-server_socket.close()
+sender.start()
+recver.start()
+
+while True:
+    time.sleep(1)
+    pass
+    
