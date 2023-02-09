9f9342ae34ce1ec24322b3fa8d88b3729d2007c5
sourcejung <sourcejung@gmail.com>
2023-02-09 17:08:08 +0900

    음성데이터 받기테스트

---
+++ test_voice/test1.py
@@ -0,0 +1,17 @@
+import pyaudio
+import numpy as np
+ 
+CHUNK = 2**8
+RATE = 44100
+ 
+p=pyaudio.PyAudio()
+stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
+              frames_per_buffer=CHUNK,input_device_index=2)
+ 
+while(True):
+    data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
+    print(int(np.average(np.abs(data))))
+ 
+stream.stop_stream()
+stream.close()
+p.terminate()
