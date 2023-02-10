4d7129aa09dc55434f63039f48c5d2cfe9a3d726
sourcejung <sourcejung@gmail.com>
2023-02-10 14:29:14 +0900

    음성데이터 받은다음 스피커 출력

--- test_voice/test1.py
+++ test_voice/test1.py
@@ -1,17 +1,24 @@
 import pyaudio
 import numpy as np
  
-CHUNK = 2**8
-RATE = 44100
- 
-p=pyaudio.PyAudio()
-stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
-              frames_per_buffer=CHUNK,input_device_index=2)
- 
-while(True):
-    data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
-    print(int(np.average(np.abs(data))))
- 
-stream.stop_stream()
-stream.close()
-p.terminate()
+def reacorder(stream):
+    data = stream.read(1024)
+    return data
+
+def speaker(stream,data):
+    stream.write(data)
+    
+def send():
+    pass
+
+mic = pyaudio.PyAudio()
+mic_stream = mic.open(format=pyaudio.paFloat32,channels=1,rate=44100,input=True,frames_per_buffer=1024)
+
+speaker_obj = pyaudio.PyAudio()
+speaker_stream = speaker_obj.open(format=pyaudio.paFloat32,channels=1,rate=44100,output=True,frames_per_buffer=1024)
+
+while True:
+    data = reacorder(mic_stream)
+    data = np.frombuffer(data,np.single)
+    data = data
+    speaker(speaker_stream,data.tobytes())
