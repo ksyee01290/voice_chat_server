bca6be2a4c68ec164a6e6fd61cf78a10c77cc059
sourcejung <sourcejung@gmail.com>
2023-02-17 16:09:20 +0900

    브로드캐스팅 테스트

--- test_voice/test_server.py
+++ test_voice/test_server.py
@@ -3,53 +3,71 @@ import numpy as np
 import socket
 import threading
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt8
+CHUNK = 256
+FORMAT = pyaudio.paFloat32
 CHANNELS = 1
 RATE = 48000
 
-def recorder(stream):
-    data = stream.read(CHUNK, exception_on_overflow=False)
-    return data
+def writer(sock, mic_stream):
+    while True:
+        data = sound.recorder(mic_stream)
+        net.send(client_socket, bytes(data))
 
-def speaker(stream, data):
-    stream.write(data)
-
-def receive(client_socket):
-    return client_socket.recv(CHUNK)
-
-def send(client_socket, data):
-    client_socket.sendall(data)
+def reader(sock, speaker_stream):
+    while True:
+        data = net.receive(client_socket)
+        sound.speaker(speaker_stream, data)
 
 def handle_client(client_socket):
-    speaker_obj = pyaudio.PyAudio()
-    speaker_stream = speaker_obj.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
-    mic = pyaudio.PyAudio()
-    mic_stream = mic.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-
-    try:
-        while True:
-            data = recorder(mic_stream)
-            send(client_socket, bytes(data))
-            data = receive(client_socket)
-            speaker(speaker_stream, data)
-            
-    except (ConnectionResetError, BrokenPipeError):
-        print(f"Client {client_socket.getpeername()} disconnected.")
-    finally:
-        client_socket.close()
-        speaker_stream.close()
-        speaker_obj.terminate()
-        mic_stream.close()
-        mic.terminate()
+    sound_obj = pyaudio.PyAudio()
+    speaker_stream = sound_obj.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
+    mic_stream = sound_obj.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
         
+    writer_thread = threading.Thread(target=writer, args=(client_socket,mic_stream))
+    reader_thread = threading.Thread(target=reader, args=(client_socket,speaker_stream))
+    
+    writer_thread.start()
+    reader_thread.start()
+    writer_thread.join()
+    reader_thread.join()
+    
+class Sound:
+    def recorder(stream):
+        data = stream.read(CHUNK, exception_on_overflow=False)
+        return data
+
+    def speaker(stream, data):
+        stream.write(data)
+
+class Net:
+    def receive(client_socket):
+        length = CHUNK*4
+        buf = []
+        while True:
+            buf += client_socket.recv(length)
+            if len(buf)>=length:
+                break
+        return bytes(buf)
+
+    def send(client_socket, data):
+        client_socket.sendall(data)
+        
+sound = Sound
+net = Net
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('127.0.0.1', 9999))
-server_socket.listen(5)
+server_socket.bind(('127.0.0.1', 10002))
+server_socket.listen(3)
+
+client = []
 
 while True:
+    
     client_socket, address = server_socket.accept()
     print('연결 되었다 : ', address)
     
+    client.append(client_socket)
     client_thread = threading.Thread(target=handle_client, args=(client_socket,))
     client_thread.start()
+
+    
